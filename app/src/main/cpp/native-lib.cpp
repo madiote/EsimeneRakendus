c8d45ef6200908fbf27e79f18120bb6eda03450d
Madis Otenurm <43170891+madiote@users.noreply.github.com>
2020-03-10 18:25:25 +0200

    Esimenerakendus2

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,13 +1,13 @@
 #include <jni.h>
 #include <random>
-
+// Traditional c++ function
 auto randomNumber(int start, int end){
     std::random_device r;
     std::default_random_engine en(r());
     std::uniform_int_distribution<int> dist(start, end);
     return dist(en);
 }
-
+// JNI wrapper function
 extern "C" JNIEXPORT jint JNICALL
 Java_com_tlu_esimenerakendus_MainActivity_randomDice(
         JNIEnv* env,
